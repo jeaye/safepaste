929ac483a9e8205dde091cc8c13415e8a069c4a1
jeaye <contact@jeaye.com>
2016-01-23 18:17:27 +0800

    Add essential AES 256 encryption bits

--- src/safepaste/core.clj
+++ src/safepaste/core.clj
@@ -1,7 +1,40 @@
 (ns safepaste.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.data.codec.base64 :as b64])
+  (:import (javax.crypto Cipher KeyGenerator SecretKey)
+           (javax.crypto.spec SecretKeySpec)
+           (java.security SecureRandom)))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn string->bytes [s]
+  (.getBytes s "UTF-8"))
+
+(defn base64 [b]
+  (String. (b64/encode b) "UTF-8"))
+
+(defn debase64 [b]
+  (String. (b64/decode b) "UTF-8"))
+
+(defn get-raw-key [seed]
+  (let [keygen (KeyGenerator/getInstance "AES")
+        sr (SecureRandom/getInstance "SHA1PRNG")]
+    (.setSeed sr (string->bytes seed))
+    (.init keygen 128 sr)
+    (.. keygen generateKey getEncoded)))
+
+(defn get-cipher [mode seed]
+  (let [key-spec (SecretKeySpec. (get-raw-key seed) "AES")
+        cipher (Cipher/getInstance "AES")]
+    (.init cipher mode key-spec)
+    cipher))
+
+(defn encrypt [text key]
+  (let [bytes (string->bytes text)
+        cipher (get-cipher Cipher/ENCRYPT_MODE key)]
+    (base64 (.doFinal cipher bytes))))
+
+(defn decrypt [text key]
+  (let [cipher (get-cipher Cipher/DECRYPT_MODE key)]
+    (String. (.doFinal cipher (debase64 (string->bytes text))))))
+
+(defn -main [& args]
+  (println (encrypt "message" "key")))
