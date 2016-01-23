cafd609b212b872c0e9f9c37162d5c3f0dc0d461
jeaye <contact@jeaye.com>
2016-01-23 22:15:54 +0800

    Switch to base 16 for output

--- src/safepaste/core.clj
+++ src/safepaste/core.clj
@@ -5,24 +5,37 @@
             [buddy.core.nonce :as nonce]
             [buddy.core.hash :as hash]))
 
-(def message
-  (codecs/str->bytes "Hello World."))
+(defn hexify [b]
+  (apply str (map #(format "%02x" %) b)))
 
-(def iv (nonce/random-bytes 16))
-(def secret-key (nonce/random-bytes 64))
+(defn unhexify [s]
+  (let [bytes (into-array Byte/TYPE
+                          (map (fn [[x y]]
+                                 (unchecked-byte (Integer/parseInt (str x y) 16)))
+                               (partition 2 s)))]
+    bytes))
 
-(def encrypted (crypto/encrypt message
-                               secret-key
-                               iv
-                               {:algorithm :aes256-cbc-hmac-sha512}))
+(def iv (codecs/str->bytes "1234567890123456"))
 
-(def decrypted
-  (-> (crypto/decrypt encrypted
-                      secret-key
+(defn encrypt [data]
+  (let [random-iv iv
+        random-key (nonce/random-bytes 64)]
+    [random-key
+     (crypto/encrypt (codecs/str->bytes data)
+                    random-key
+                    random-iv
+                    {:algorithm :aes256-cbc-hmac-sha512})]))
+
+(defn decrypt [file]
+  (-> (crypto/decrypt (unhexify (slurp file))
+                      (unhexify file)
                       iv
                       {:algorithm :aes256-cbc-hmac-sha512})
       (codecs/bytes->str)))
 
 (defn -main [& args]
-  (println (str encrypted))
-  (println decrypted))
+  (if (some #(= % "-d") args)
+    (println (decrypt (second args)))
+    (let [[random-key encrypted-data] (encrypt (first args))]
+      (println (hexify random-key))
+      (spit (hexify random-key) (hexify encrypted-data)))))
