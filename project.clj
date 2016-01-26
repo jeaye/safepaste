980474f4edaf490c75cde819aad4aae905bf5c86
jeaye <contact@jeaye.com>
2016-01-26 18:40:53 +0800

    Hook in ring and compojure

--- project.clj
+++ project.clj
@@ -4,7 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [buddy/buddy-core "0.9.0"]]
+                 [buddy/buddy-core "0.9.0"]
+
+                 [compojure "1.4.0"]
+
+                 [ring/ring-core "1.4.0" :exclusions [javax.servlet/servlet-api]]
+                 [ring/ring-servlet "1.4.0" :exclusions [javax.servlet/servlet-api]]
+                 [ring/ring-defaults "0.1.5" :exclusions [javax.servlet/servlet-api]]
+
+                 [cc.qbits/jet "0.5.4"]]
   :main ^:skip-aot safepaste.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/safepaste/core.clj
+++ src/safepaste/core.clj
@@ -3,7 +3,14 @@
   (:require [buddy.core.crypto :as crypto]
             [buddy.core.codecs :as codecs]
             [buddy.core.nonce :as nonce]
-            [buddy.core.hash :as hash]))
+            [buddy.core.hash :as hash]
+
+            [qbits.jet.server :refer [run-jetty]]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [compojure.response :refer [render]]
+            [clojure.java.io :as io]))
 
 (defn hexify [b]
   (apply str (map #(format "%02x" %) b)))
@@ -33,9 +40,27 @@
                       {:algorithm :aes256-cbc-hmac-sha512})
       (codecs/bytes->str)))
 
-(defn -main [& args]
-  (if (some #(= % "-d") args)
-    (println (decrypt (second args)))
-    (let [[random-key encrypted-data] (encrypt (first args))]
-      (println (hexify random-key))
-      (spit (hexify random-key) (hexify encrypted-data)))))
+;(defn -main [& args]
+;  (if (some #(= % "-d") args)
+;    (println (decrypt (second args)))
+;    (let [[random-key encrypted-data] (encrypt (first args))]
+;      (println (hexify random-key))
+;      (spit (hexify random-key) (hexify encrypted-data)))))
+
+;; This is a handler that returns the
+;; contents of `resources/index.html`
+(defn home
+  [req]
+  (render (io/resource "index.html") req))
+
+;; Defines a handler that acts as router
+(defroutes app
+  (GET "/" [] home)
+  (route/resources "/static")
+  (route/not-found "<h1>Page not found</h1>"))
+
+;; Application entry point
+(defn -main
+  [& args]
+  (let [app (wrap-defaults app site-defaults)]
+    (run-jetty {:ring-handler app :port 5050})))
