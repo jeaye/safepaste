bb5b301f07f3fb4ab2dcf59b061320e8f186bc15
jeaye <contact@jeaye.com>
2016-01-30 23:14:34 +0800

    Add a donate page

--- src/cljs/safepaste/core.cljs
+++ src/cljs/safepaste/core.cljs
@@ -7,6 +7,8 @@
 
 (def about-post
   "/about---#439d738e6e11e2583c9d2414541d7cae6b612a524c4af930a4fdedaeabccd31b")
+(def donate-post
+  "/donate--#6930b5545b073d1345d03cc6adf5409d58ba9ecc49b5b4fabb72fcaed0c8c1e8")
 
 (defn view-post! [path]
   (dom/set-url! path)
@@ -20,6 +22,7 @@
     (remote/get!))
   (dommy/listen! (sel1 :#new) :click dom/reset-page!)
   (dommy/listen! (sel1 :#about) :click (fn [_] (view-post! about-post)))
+  (dommy/listen! (sel1 :#donate) :click (fn [_] (view-post! donate-post)))
   (dommy/listen! (sel1 :#post) :click remote/post!))
 
 ; TODO: listen to browser back/forward and refresh everything
